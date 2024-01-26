2d4c682764e882c8dc794514ef89fa9133a270d3
CaioBalc <balczarekcaio@gmail.com>
2024-01-26 11:52:16 -0300

    disciplinas

--- classes/professor.php
+++ classes/professor.php
@@ -2,17 +2,17 @@
 
 class Professor extends AbstractPessoa// Com disciplina 1:n. 
 {
-    protected array $disciplina; // Array obj. disciplina
+    protected array $disciplinas; // Array obj. disciplina
 
     public function __construct(string $nome, int $idade, array $disciplina)
     {
         parent::__construct($nome, $idade, $disciplina);
-        $this->disciplina = $disciplina;
+        $this->disciplinas = $disciplina;
     }
 
     # Função para impressão de professores
     public function __toString() {
-        $disciplinasArray = implode(', ', $this->disciplina);
+        $disciplinasArray = implode(', ', $this->disciplinas);
         return "Nome: {$this->nome},\nIdade: {$this->idade} anos\nDisciplinas: {$disciplinasArray}\n\n";
     }
 }
